35cb8467007d3bd04064744d4ca2a86629d10335
jvleon <jvleon95@outlook.es>
2017-11-30 14:09:19 -0600

    creating order view

---
+++ YUMRestaurant/components/orderPreview/index.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components/native'
+import React from 'react'
+
+const MainContainer = styled.View`
+  width: 100%;
+  height: 50%;
+`
+const LeftContainer = styled.View`
+  flex: 1;
+`
+const RightContainer = styled.View`
+  flex: 1;
+`
+const OrangeText = styled.Text`
+  font-size: 20px;
+  color: #FF572;
+`
+const GrayText =  styled.Text`
+  font-size: 15px;
+  color: #C7C7CC;
+`
+const RawText = styled.View`
+  font-size: 20px;
+`
+const OrderContainer = styled.View`
+  flex: 1;
+`
+const StatusContainer = styled.View`
+  flex: 1;
+`
+const
+const OrderPreview = () =>(
+  <MainContainer>
+    <LeftContainer>
+
+    </LeftContainer>
+    <RightContainer></RightContainer>
+  </MainContainer>
+)
+ export default OrderPreview
--- YUMRestaurant/routes/index.js
+++ YUMRestaurant/routes/index.js
@@ -1,10 +1,10 @@
 import {StackNavigator} from 'react-navigation'
 import React,{Component} from 'react'
 import Example from '../containers/example'
-
+import {Prueba} from '../containers'
 
 export const AppStack = StackNavigator({
-  Example: {
-    screen: Example,
+  Prueba: {
+    screen: Prueba,
   },
 });
